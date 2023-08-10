7223d5985c47cfb4481316f5f92ae8ac9b395a32
risav pyakurel <pyakurel.risav@gmail.com>
2023-08-10 17:48:20 +0545

    path parameters updated

--- main.py
+++ main.py
@@ -3,13 +3,36 @@ from fastapi.params import Body
 
 app = FastAPI()
 
-
-
+'''
+# decorator
 @app.get("/") # / is a root path
-def index():
-    return {'data':{'name':'RISAV'}}
+def index(): 
+    return {'data':{'name':'RISAV'}} #path operation function
 
 @app.get('/about')
 def about():
     return {'data': 'about page loaded'}
 
+'''
+
+@app.get("/")
+
+def index():
+    return{'data' : 'blog list'}
+    
+@app.get('/blog/unpublished')
+def unpublished():
+    return{'data': 'unpublished blogs'}
+
+@app.get('/blog/{id}')
+def show(id: int):
+    # fetch blog with id =id
+    return {'data': id}
+
+
+
+@app.get('/blog/{id}/comments')
+
+def comments(id):
+    #fetch comments of blog with id = id
+    return{'data': {'1', '2'}}
